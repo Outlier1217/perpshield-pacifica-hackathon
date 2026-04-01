d228c92f7fbb8383fe3f509975abbd6819d8e49c
Outlier1217 <mustakaalam10@gmail.com>
2026-04-01 15:15:12 +0530

    Updated deposit and withdraw testing scripts

--- perpshield/scripts/testDeposit.js
+++ perpshield/scripts/testDeposit.js
@@ -3,8 +3,8 @@ const { ethers } = require("hardhat");
 async function main() {
   const [user] = await ethers.getSigners();
 
-  const usdcAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-  const vaultAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  const usdcAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const vaultAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
   const usdc = await ethers.getContractAt("MockUSDC", usdcAddress);
   const vault = await ethers.getContractAt("PerpShieldVault", vaultAddress);
--- perpshield/scripts/testWithdraw.js
+++ perpshield/scripts/testWithdraw.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 async function main() {
   const [user] = await ethers.getSigners();
 
-  const vaultAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  const vaultAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
   const vault = await ethers.getContractAt("PerpShieldVault", vaultAddress);
 
