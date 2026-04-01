9c64802f88553a3d37ce565ff15fcf83b3fc8896
Outlier1217 <mustakaalam10@gmail.com>
2026-04-01 19:31:40 +0530

    Set up core React app structure

---
+++ perpshield-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import VaultCard from "./components/VaultCard";
+import ShieldScore from "./components/ShieldScore";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <ShieldScore />
+      <VaultCard />
+    </>
+  );
+}
+
+export default App;
